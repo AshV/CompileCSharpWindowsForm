f76958c9a0dc922a5bde93b9282d2e746a281e60
Ashish Vishwakarma <Ashish.Vishwakarma@hsd.com.au>
2017-01-16 03:41:45 +0530

    Moving to Github

---
+++ CompileCSharp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CompileCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Build = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Build
+            // 
+            this.Build.Location = new System.Drawing.Point(12, 341);
+            this.Build.Name = "Build";
+            this.Build.Size = new System.Drawing.Size(289, 23);
+            this.Build.TabIndex = 0;
+            this.Build.Text = "Build";
+            this.Build.UseVisualStyleBackColor = true;
+            this.Build.Click += new System.EventHandler(this.Build_Click);
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(310, 341);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(289, 23);
+            this.Run.TabIndex = 1;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(587, 323);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 370);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(587, 44);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 426);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Build);
+            this.Name = "Form1";
+            this.Text = "Compile C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Build;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CompileCSharp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CompileCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Build.Click += new System.EventHandler(this.Build_Click);
+            this.Run.Click += new System.EventHandler(this.Build_Click);
+        }
+
+        private void Build_Click(object sender, EventArgs e)
+        {
+            var codeProvider = CodeDomProvider.CreateProvider("CSharp");
+            var Output = "Out.exe";
+            var ButtonObject = (Button)sender;
+
+            textBox2.Text = "";
+            var parameters = new CompilerParameters();
+            //Make sure we generate an EXE, not a DLL
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = Output;
+            var results = codeProvider.CompileAssemblyFromSource(parameters, textBox1.Text);
+
+            if (results.Errors.Count > 0)
+            {
+                textBox2.ForeColor = Color.Red;
+                foreach (CompilerError CompErr in results.Errors)
+                {
+                    textBox2.Text = textBox2.Text +
+                                "Line number " + CompErr.Line +
+                                ", Error Number: " + CompErr.ErrorNumber +
+                                ", '" + CompErr.ErrorText + ";" +
+                                Environment.NewLine + Environment.NewLine;
+                }
+            }
+            else
+            {
+                //Successful Compile
+                textBox2.ForeColor = Color.Green;
+                textBox2.Text = "Success!";
+                //If we clicked run then launch our EXE
+                if (ButtonObject.Text == "Run") Process.Start(Output);
+            }
+        }
+    }
+}
